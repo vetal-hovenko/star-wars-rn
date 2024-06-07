4d87a2004b1183401fa9ed687b6c7bb7c1d9cd2c
HVO <HVO@d.c>
2024-06-07 15:39:16 +0300

    updated metro config

--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,7 @@
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const {getDefaultConfig} = require('@react-native/metro-config');
 
-/**
- * Metro configuration
- * https://reactnative.dev/docs/metro
- *
- * @type {import('metro-config').MetroConfig}
- */
-const config = {};
+const defaultConfig = getDefaultConfig(__dirname);
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+defaultConfig.resolver.sourceExts.push('mjs');
+
+module.exports = defaultConfig;
